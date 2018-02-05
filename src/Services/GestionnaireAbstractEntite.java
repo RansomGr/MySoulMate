163bea138cefdf32384ad5ae2c43f7637ccefa15
RansomGr <ahmedamine.benmimoun@esprit.tn>
2018-02-05 22:00:31 +0100

    Initial commit

--- src/Services/GestionnaireAbstractEntite.java
+++ src/Services/GestionnaireAbstractEntite.java
@@ -5,44 +5,58 @@
  */
 package Services;
 
+import Entites.AbstractEntite;
+
+import static Services.Gestionnaire.DB;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
-import java.util.List;
+
 
 /**
  *
  * @author Ransom
  */
-public class GestionnaireAbstractEntite implements Gestionnaire {
+public abstract class GestionnaireAbstractEntite implements Gestionnaire {
 
     @Override
     public int create(Object o) throws SQLException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        AbstractEntite e = (AbstractEntite)o;// down Cast
+        String query=" insert into Entite (ID,nom) values (?,?) "; // preparation du query
+
+         PreparedStatement pst=DB.prepareStatement(query);// Recuperation de l'objet PreparedStatment
+         
+         pst.setInt(1, e.getID());// Binding du premier valeur mentionner dans le query "?" 
+         pst.setString(2, e.getNom());//Binding du deuxieme valeur mentionner dans le query "?" 
+   
+         return pst.executeUpdate(); // Execution et retour du resultat du query 
     }
 
     @Override
     public int update(Object o) throws SQLException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+      
+        AbstractEntite e = (AbstractEntite)o;// down Cast du Object => Reclamation 
+        String query=" update  Entite set ID=?,nom=? where ID=? "; // preparation du query
+
+         PreparedStatement pst=DB.prepareStatement(query);// Recuperation de l'objet PreparedStatment
+         
+         pst.setInt(1, e.getID());// Binding du premier valeur mentionner dans le query "?" 
+         pst.setString(2, e.getNom());//Binding du deuxieme valeur mentionner dans le query "?" 
+         
+         pst.setInt(3, e.getID());//Binding du valeur de l'id mentionné dans le query "?" 
+         return pst.executeUpdate(); // Execution et retour du resultat du query 
     }
 
     @Override
     public int remove(Object o) throws SQLException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+         AbstractEntite e = (AbstractEntite)o;// down Cast du Object => Reclamation 
+        String query=" delete from  Entite  where ID=? "; // preparation du query delete 
+
+         PreparedStatement pst=DB.prepareStatement(query);// Recuperation de l'objet PreparedStatment
+         
+         pst.setInt(1, e.getID());//Binding du valeur de l'id mentionné dans le query "?" 
+         return pst.executeUpdate(); // Execution et retour du resultat du query 
     }
 
-    @Override
-    public List<? extends Object> fetchAll() throws SQLException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public List<? extends Object> fetchAll(String aux, int target_column, String OrderBy) throws SQLException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public List<? extends Object> fetchAll(String aux, int target_column, String OrderBy, int startPoint, int breakPoint) throws SQLException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-    
+   
     
 }
