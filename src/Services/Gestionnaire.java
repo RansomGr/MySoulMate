0d3159a188342b47a8c9d28139f16fa06d9724e0
RansomGr <ahmedamine.benmimoun@esprit.tn>
2018-05-20 13:55:25 +0100

    Merge branch 'master' of https://github.com/RansomGr/MySoulMate-Mobile-
    # Conflicts:
    #	codenameone_settings.properties
    #	src/Views/User/LoginView.java

--- src/Services/Gestionnaire.java
+++ src/Services/Gestionnaire.java
@@ -10,33 +10,29 @@ import DataBase.Singleton;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.List;
+import java.util.Map;
 
 /**
  *
  * @author Ransom
+ * @param <T>
  */
-public  interface Gestionnaire 
+public  interface Gestionnaire <T>
 {
     // Call of Singleton 
     final Connection DB=Singleton.getInstence();
     /*#Start --- CRUD----- */
-    public int create(Object o)throws SQLException;
-    public int update(Object o)throws SQLException;
-    public int remove(Object o)throws SQLException;// A.K.A delete
-    public List<? extends Object>fetchAll()throws SQLException;// A.K.A Display
+    public int create(T o)throws SQLException;
+    public int update(T o)throws SQLException;
+    public int remove(T o)throws SQLException;// A.K.A delete
+    public List<T>  fetchAll()throws SQLException;// A.K.A Display
       /*#End --- CRUD----- */
     
-      /*#Start --- SpecialFunctions----- */
-    public List<? extends Object>fetchAll(String aux,int target_column,String OrderBy)throws SQLException;/*a simple order by fetch with column sensitivity 
-    the aux variable is for the like alias in the query to compare individual columns ( AKA recherche dynamique ) 
-    */
-    
-    public List<? extends Object>fetchAll(String aux,int target_column,String OrderBy,int startPoint,int breakPoint)throws SQLException;/*this function will fetch a sorted 
-    list of your table with a limiter that starts from an index to the limit index ;
-    example the first 20 rows of your table ordered by date the startPoint = 0 the breakPoint will be 20
-    */
-
-      /*#End --- SpecialFunctions----- */
+    public T fetchOneById(int id); // single fetch 
+    public T fetchOnByCriteria(Map<String,String> Criteras);// where Key is the name of the column and value is the value of the column
+    public List<T>fetchSomeBy(String aux, String target_column ,int StartPoint,int BreakPoint);// limited and one column is questioned
+    public List<T>fetchSomeBy(String aux, int target_column);// no limit one column is questioned
+    public List<T>fetchSomeBy(String aux,int StartPoint,int BreakPoint);// limited rows all Columns are questioned 
     
     
 }
