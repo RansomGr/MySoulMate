890c9946cba082ae552f1e8e39f2c889a4ef425b
AmiraBalti <36224123+AmiraBalti@users.noreply.github.com>
2018-05-20 15:26:55 +0100

    Merge branch 'master' of https://github.com/RansomGr/MySoulMate-Mobile-
    # Conflicts:
    #	src/Service/Service.java
    #	src/Views/Events/EventsListerView.java
    #	src/Views/MainFrameView.java
    #	src/Views/Matching/UtilisateurView.java
    #	src/Views/Matching/VoirMatchingView.java

--- src/Services/Events/GestionnaireInviteEvent.java
+++ src/Services/Events/GestionnaireInviteEvent.java
@@ -7,7 +7,7 @@ package Services.Events;
 
 import Entites.Events.Events;
 import Entites.Events.InviteEvents;
-import Entites.User.Client;
+import Entites.User.Utilisateur;
 import Services.Gestionnaire;
 import static Services.Gestionnaire.DB;
 import Services.User.GestionnaireClient;
@@ -16,6 +16,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 /**
  *
@@ -23,26 +24,24 @@ import java.util.List;
  */
 public class GestionnaireInviteEvent implements Gestionnaire{
 
-    @Override
-    public int create(Object o) throws SQLException {
+    public int create(InviteEvents o) throws SQLException {
     InviteEvents ivtevt=(InviteEvents)o;
-      String query="insert into invite_evenement(evenement_groupe,client) values(?,?)";
+      String query="insert into invite_evenement(evenement_groupe,client) values(?,?,'non')";
       PreparedStatement pst= DB.prepareStatement(query);
-      pst.setInt(1, ivtevt.getEvts().getID());
-      pst.setInt(2,ivtevt.getInvite().getID());
+      pst.setInt(1, ivtevt.getEvts().getId());
+      pst.setInt(2,ivtevt.getInvite().getId());
            
       return pst.executeUpdate();    
     }
 
-    @Override
-    public int update(Object o) throws SQLException {
-        InviteEvents inevt=(InviteEvents)o;
+    public int update(InviteEvents o) throws SQLException {
+        InviteEvents inevt=o;
         String query ="update invite_evenement set events=?,client=?  where events=? and client=? ";
       
       PreparedStatement pst=DB.prepareStatement(query);
       
-        pst.setInt(1, inevt.getEvts().getID());
-        pst.setInt(2,inevt.getInvite().getID());
+        pst.setInt(1, inevt.getEvts().getId());
+        pst.setInt(2,inevt.getInvite().getId());
 //        pst.setInt(3, inevt.getEvtgroup().getID());
 //        pst.setInt(4,inevt.getClt().getID());
        return pst.executeUpdate();  
@@ -55,8 +54,8 @@ public class GestionnaireInviteEvent implements Gestionnaire{
     
     PreparedStatement pst=DB.prepareStatement(query);
     
-     pst.setInt(1, inevt.getEvts().getID());
-     pst.setInt(2,inevt.getInvite().getID());
+     pst.setInt(1, inevt.getEvts().getId());
+     pst.setInt(2,inevt.getInvite().getId());
     
     return pst.executeUpdate();}
 
@@ -66,30 +65,65 @@ public class GestionnaireInviteEvent implements Gestionnaire{
           PreparedStatement pst=DB.prepareStatement(query);// Preparation du requete et  recuperation de l'objet Prepared statment
           List<InviteEvents>InviteEvent = new ArrayList<>();//  Creation du List Reclamation
           ResultSet res = pst.executeQuery();// execution du query et recuperation du result set
-          List<Client>Clients ;
+          List<Utilisateur>Clients ;
           List<Events>events;
           GestionnaireEvent g = new GestionnaireEvent();
           GestionnaireClient c= new GestionnaireClient();
-          Clients=(List<Client>) c.fetchAll();
+          Clients=(List<Utilisateur>) c.fetchAll();
           events=(List<Events>) g.fetchAll();
           while(res.next())// parcour du result set
           {
               int Client_ID_in_question=res.getInt(2);
               int Event_ID_in_question=res.getInt(1);
-              Client clt =Clients.stream().filter(c1->c1.getID()==Client_ID_in_question).findFirst().get();
-              Events evt = events.stream().filter(e1->e1.getID()==Event_ID_in_question).findFirst().get();
+              Utilisateur clt =Clients.stream().filter(c1->c1.getId()==Client_ID_in_question).findFirst().get();
+              Events evt = events.stream().filter(e1->e1.getId()==Event_ID_in_question).findFirst().get();
              InviteEvent.add(new InviteEvents(evt,clt));
            }
           return InviteEvent;
     }
 
+//    @Override
+//    public List<? extends Object> fetchAll(String aux, int target_column, String OrderBy) throws SQLException {
+//        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+//    }
+//
+//    @Override
+//    public List<? extends Object> fetchAll(String aux, int target_column, String OrderBy, int startPoint, int breakPoint) throws SQLException {
+//        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+//    }
+
     @Override
-    public List<? extends Object> fetchAll(String aux, int target_column, String OrderBy) throws SQLException {
+    public Object fetchOneById(int id) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
-    public List<? extends Object> fetchAll(String aux, int target_column, String OrderBy, int startPoint, int breakPoint) throws SQLException {
+    public Object fetchOnByCriteria(Map Criteras) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List fetchSomeBy(String aux, String target_column, int StartPoint, int BreakPoint) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List fetchSomeBy(String aux, int target_column) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List fetchSomeBy(String aux, int StartPoint, int BreakPoint) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public int create(Object o) throws SQLException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public int update(Object o) throws SQLException {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
     
